98a331b6dbaf84c7085c400a93b5236ba89fc18b
harurunn <harurunn.jp@gmail.com>
2018-07-26 09:35:08 +0900

    test

--- app/src/main/java/com/harurunn/aplication/todo_manager_android/HomeActivity.kt
+++ app/src/main/java/com/harurunn/aplication/todo_manager_android/HomeActivity.kt
@@ -8,7 +8,6 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
     }
 }
 
